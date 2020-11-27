c82d9acd2d49b06754baa92ff0a28203fe061629
unknown <greyx537@gmail.com>
2020-11-27 18:56:34 +0700

    memfilter jenis soal agar tidka double

--- views/pelatihan/_soal_jenis.php
+++ views/pelatihan/_soal_jenis.php
@@ -1,5 +1,12 @@
-<?=$form->field($modelSoalJenis, "jenis_id")->dropdownList(
-    \yii\helpers\ArrayHelper::map(app\models\MasterJenisSoal::find()->all(), 'id', 'nama'),
+<?php
+
+    $exist = [];
+    $modelCheckExist = app\models\PelatihanSoalJenis::find()->where(['pelatihan_id' => $modelSoalJenis->pelatihan_id])->select('jenis_id')->all();
+    foreach($modelCheckExist as $data){
+        array_push($exist, $data->jenis_id);
+    }
+    echo $form->field($modelSoalJenis, "jenis_id")->dropdownList(
+    \yii\helpers\ArrayHelper::map(app\models\MasterJenisSoal::find()->where(['not in', 'id', $exist])->all(), 'id', 'nama'),
     [
         'prompt' => 'Select',
     ])?>
