60d328312bd801aef4640056a13f5af6c59c81be
unknown <greyx537@gmail.com>
2020-12-02 12:41:18 +0700

    merubah template

--- views/pelatihan/_info.php
+++ views/pelatihan/_info.php
@@ -2,13 +2,22 @@
 
 use app\components\RoleType;
 
+$colomn_2 = [
+    'template' => '
+    <div class="col-lg-12">
+        <div class="col-md-4"><label class="control-label">{labelTitle}</label></div>
+        <div class="col-md-8">{input}</div>
+        <div class="col-md-12">{error}</div>
+    </div>
+        ',
+    'options'=>['class'=>'col-md-6', 'style'=>'padding:0px;']
+];
+
 ?>
 
 			<?= $form->field($model, 'id', ['template' => '{input}'])->textInput(['style' => 'display: none']) ?>
-            <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
-			<?= $form->field($model, 'latar_belakang')->textarea(['rows' => 6]) ?>
-			<?= $form->field($model, 'tujuan')->textarea(['rows' => 6]) ?>
-            <?= $form->field($model, 'tanggal_mulai')->widget(
+            <?= $form->field($model, 'nama', $colomn_2)->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'tanggal_mulai', $colomn_2)->widget(
                 \kartik\date\DatePicker::className(),
                 [
                     'pluginOptions' => [
@@ -17,7 +26,7 @@ use app\components\RoleType;
                     ]
                 ]
             ) ?>
-			<?= $form->field($model, 'tanggal_selesai')->widget(
+			<?= $form->field($model, 'tanggal_selesai', $colomn_2)->widget(
                 \kartik\date\DatePicker::className(),
                 [
                     'pluginOptions' => [
@@ -26,9 +35,11 @@ use app\components\RoleType;
                     ]
                 ]
             ) ?>
-            <?= $form->field($model, 'kriteria')->textInput() ?>
-            <?= $form->field($model, 'jumlah_target')->textInput(['type' => 'number', 'min' => 1]) ?>
-            <?= $form->field($model, 'sasaran_wilayah')->textInput() ?>
+            <?= $form->field($model, 'kriteria', $colomn_2)->textInput() ?>
+			<?= $form->field($model, 'latar_belakang', $colomn_2)->textarea(['rows' => 6]) ?>
+			<?= $form->field($model, 'tujuan', $colomn_2)->textarea(['rows' => 6]) ?>
+            <?= $form->field($model, 'jumlah_target', $colomn_2)->textInput(['type' => 'number', 'min' => 1]) ?>
+            <?= $form->field($model, 'sasaran_wilayah', $colomn_2)->textInput() ?>
             <?php
             // $form->field($model, 'tingkat_id')->dropDownList(
             //     \yii\helpers\ArrayHelper::map(app\models\PelatihanTingkat::find()->all(), 'id', 'nama'),
@@ -39,17 +50,17 @@ use app\components\RoleType;
             // );
             ?>
             
-            <?= $form->field($model, 'lokasi')->textInput(['maxlength' => true]) ?>
-            <?= $form->field($model, 'kota')->textInput(['maxlength' => true]) ?>
-            <?= $form->field($model, 'nama_penandatangan')->textInput(['maxlength' => true]) ?>
-            <?= $form->field($model, 'nip_penandatangan')->textInput(['maxlength' => true]) ?> 
-            <?= $form->field($model, 'forum_diskusi')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'lokasi', $colomn_2)->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'kota', $colomn_2)->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'nama_penandatangan', $colomn_2)->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'nip_penandatangan', $colomn_2)->textInput(['maxlength' => true]) ?> 
+            <?= $form->field($model, 'forum_diskusi', $colomn_2)->textInput(['maxlength' => true]) ?>
 
             <?php
             if (Yii::$app->user->identity->role_id == RoleType::SA) :
-                echo $form->field($model, 'pelaksana_id')->dropDownList(
+                echo $form->field($model, 'pelaksana_id', $colomn_2)->dropDownList(
                     \yii\helpers\ArrayHelper::map(app\models\User::find()->where([
-                        '=!', 'role_id', '3' // role peserta
+                        '!=', 'role_id', 3 // role peserta
                     ])->all(), 'id', 'name'),
                     [
                         'prompt' => 'Select',
