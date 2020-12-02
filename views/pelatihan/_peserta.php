9e50003c844875586ce9ab480a07dee06453b809
unknown <greyx537@gmail.com>
2020-12-03 06:39:49 +0700

    addbtn add in panel head

--- views/pelatihan/_peserta.php
+++ views/pelatihan/_peserta.php
@@ -57,6 +57,8 @@ DynamicFormWidget::begin([
                 <div class="panel-heading">
                     <h3 class="panel-title pull-left">Peserta</h3>
                     <div class="pull-right">
+                        <button type="button" class="add-item btn btn-success btn-xs pull-right" style="margin-left: 10px;"><i
+                            class="glyphicon glyphicon-plus"></i></button>
                         <button type="button" class="remove-item btn btn-danger btn-xs"><i
                                 class="glyphicon glyphicon-minus"></i></button>
                     </div>
