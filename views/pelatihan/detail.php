fc9eed54c86101eb3702850b78ab954ef459d8df
Defri Indra Mahardika <defrindr@gmail.com>
2020-12-28 23:45:29 +0700

    clean up

--- views/pelatihan/detail.php
+++ views/pelatihan/detail.php
@@ -3,11 +3,10 @@
 /* @var $this yii\web\View */
 
 use app\components\Constant;
-use app\models\Pelatihan;
 use app\models\PelatihanSoalPeserta;
 use app\models\User;
-use yii\helpers\Url;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = 'Pelatihan : ' . $model->nama;
 ?>
@@ -15,105 +14,106 @@ $this->title = 'Pelatihan : ' . $model->nama;
 <div class="site-index">
     <div class="row">
         <?php if ($model):
-            $model_peserta = User::findOne(Yii::$app->user->identity->id)->getPelatihanPesertas()->where(['pelatihan_id' => $model->id])->one();
-            $sudah_selesai = $model->getPelatihanSoalJenis()->where(['jenis_id' => [Constant::SOAL_JENIS_POSTTEST, Constant::SOAL_JENIS_PRETEST]])->select('id')->asArray()->all();
-            $list_selesai = [];
-            foreach($sudah_selesai as $id) array_push($list_selesai, $id['id']);
-            $check_selesai = PelatihanSoalPeserta::find()->where(['peserta_id' => $model_peserta->id, 'jenis_soal' => $list_selesai, 'selesai' => 1])->count();
-            $data = $model->getPelatihanSoalJenis()->where(['jenis_id' => [Constant::SOAL_JENIS_POSTTEST, Constant::SOAL_JENIS_PRETEST]])->all();
-            // var_dump($list_selesai);
-            // die;
-            if($check_selesai == 2){
-                $data = $model->getPelatihanSoalJenis()->all();
-            }
-            foreach ($data as $o):
-                $data = [];
-                $model_soal_peserta = PelatihanSoalPeserta::findOne(['peserta_id' => $model_peserta->id, 'jenis_soal' => $o->id]);
+    $model_peserta = User::findOne(Yii::$app->user->identity->id)->getPelatihanPesertas()->where(['pelatihan_id' => $model->id])->one();
+    $sudah_selesai = $model->getPelatihanSoalJenis()->where(['jenis_id' => [Constant::SOAL_JENIS_POSTTEST, Constant::SOAL_JENIS_PRETEST]])->select('id')->asArray()->all();
+    $list_selesai = [];
+    foreach ($sudah_selesai as $id) {
+        array_push($list_selesai, $id['id']);
+    }
 
-                switch ($o->jenis_id) {
-                    case Constant::SOAL_JENIS_PRETEST:
-                        $data['link'] = Url::to(["/pretest/$model->unique_id"]);
-                        $data['bg'] = "bg-aqua";
-                        break;
-                    case Constant::SOAL_JENIS_POSTTEST:
-                        $data['link'] = Url::to(["/posttest/$model->unique_id"]);
-                        $data['bg'] = "bg-aqua";
-                        break;
-                    case Constant::SOAL_JENIS_KUESIONER_MONEV:
-                        $data['link'] = Url::to(["/kuesioner-monev/$model->unique_id"]);
-                        $data['bg'] = "bg-purple";
-                        break;
-                    case Constant::SOAL_JENIS_KUESIONER_KEPUASAN:
-                        $data['link'] = Url::to(["/kuesioner-kepuasan/$model->unique_id"]);
-                        $data['bg'] = "bg-purple";
-                        break;
-                    default:
-                        $data['link'] = "#";
-                        $data['bg'] = "bg-aqua";
-                        break;
-                }
-                
-                if ($model_soal_peserta !== null) {
-                    $time_now = strtotime(date('Y-m-d H:i:s'));
-                    if ($time_now > strtotime($model_soal_peserta->waktu_selesai)) {
-                        $data = [
-                            "text" => "Waktu Pengerjaan telah berakhir",
-                            "bg" => "bg-red",
-                            "link" => "#",
-                            "options" => [
-                                "class" => "small-box-footer",
-                            ],
-                        ];
-                    } elseif ($model_soal_peserta->selesai) {
-                    $data = [
-                        "text" => "Anda Telah Menyelesaikan Soal Pelatihan ini.",
-                        "bg" => "bg-red",
-                        "link" => "#",
-                        "options" => [
-                            "class" => "small-box-footer",
-                        ],
-                    ];
-                } else {
-                    $data = [
-                        "text" => "Lanjutkan",
-                        "bg" => 'bg-blue',
-                        "link" => $data['link'],
-                        "options" => [
-                            "class" => "small-box-footer",
-                        ],
-                    ];
-                }
+    $check_selesai = PelatihanSoalPeserta::find()->where(['peserta_id' => $model_peserta->id, 'jenis_soal' => $list_selesai, 'selesai' => 1])->count();
+    $data = $model->getPelatihanSoalJenis()->where(['jenis_id' => [Constant::SOAL_JENIS_POSTTEST, Constant::SOAL_JENIS_PRETEST]])->all();
+    if ($check_selesai == 2) {
+        $data = $model->getPelatihanSoalJenis()->all();
+    }
+    foreach ($data as $o):
+        $data = [];
+        $model_soal_peserta = PelatihanSoalPeserta::findOne(['peserta_id' => $model_peserta->id, 'jenis_soal' => $o->id]);
 
-            } else {
+        switch ($o->jenis_id) {
+            case Constant::SOAL_JENIS_PRETEST:
+                $data['link'] = Url::to(["/pretest/$model->unique_id"]);
+                $data['bg'] = "bg-aqua";
+                break;
+            case Constant::SOAL_JENIS_POSTTEST:
+                $data['link'] = Url::to(["/posttest/$model->unique_id"]);
+                $data['bg'] = "bg-aqua";
+                break;
+            case Constant::SOAL_JENIS_KUESIONER_MONEV:
+                $data['link'] = Url::to(["/kuesioner-monev/$model->unique_id"]);
+                $data['bg'] = "bg-purple";
+                break;
+            case Constant::SOAL_JENIS_KUESIONER_KEPUASAN:
+                $data['link'] = Url::to(["/kuesioner-kepuasan/$model->unique_id"]);
+                $data['bg'] = "bg-purple";
+                break;
+            default:
+                $data['link'] = "#";
+                $data['bg'] = "bg-aqua";
+                break;
+        }
+
+        if ($model_soal_peserta !== null) {
+            $time_now = strtotime(date('Y-m-d H:i:s'));
+            if ($time_now > strtotime($model_soal_peserta->waktu_selesai)) {
                 $data = [
-                    "text" => "Mulai",
-                    "bg" => $data['bg'],
-                    "link" => $data['link'],
+                    "text" => "Waktu Pengerjaan telah berakhir",
+                    "bg" => "bg-red",
+                    "link" => "#",
                     "options" => [
                         "class" => "small-box-footer",
-                        "title" => "Konfirmasi",
-                        "data-confirm" => "Apakah anda yakin sudah siap ?",
-                        "data-method" => "POST",
                     ],
                 ];
-            }
-            
-            ?>
-			        <div class="col-lg-3 col-sm-6 col-xs-12">
-			            <!-- small box -->
-			            <div class="small-box <?=$data['bg']?>">
-			                <div class="inner">
-			                    <h3><?=$o->jenis->nama?></h3>
-			                    <p>Waktu : <?=$o->waktu_pengerjaan?> Menit</p>
-			                </div>
-			                <div class="icon">
-			                    <i class="ion ion-bag"></i>
-			                </div>
-			                <?=Html::a($data['text'], $data['link'], $data['options']);?>
-			            </div>
-			        </div>
-			        <!-- ./col -->
-			        <?php endforeach?>
+            } elseif ($model_soal_peserta->selesai) {
+            $data = [
+                "text" => "Anda Telah Menyelesaikan Soal Pelatihan ini.",
+                "bg" => "bg-red",
+                "link" => "#",
+                "options" => [
+                    "class" => "small-box-footer",
+                ],
+            ];
+        } else {
+            $data = [
+                "text" => "Lanjutkan",
+                "bg" => 'bg-blue',
+                "link" => $data['link'],
+                "options" => [
+                    "class" => "small-box-footer",
+                ],
+            ];
+        }
+
+    } else {
+        $data = [
+            "text" => "Mulai",
+            "bg" => $data['bg'],
+            "link" => $data['link'],
+            "options" => [
+                "class" => "small-box-footer",
+                "title" => "Konfirmasi",
+                "data-confirm" => "Apakah anda yakin sudah siap ?",
+                "data-method" => "POST",
+            ],
+        ];
+    }
+
+    ?>
+					        <div class="col-lg-3 col-sm-6 col-xs-12">
+					            <!-- small box -->
+					            <div class="small-box <?=$data['bg']?>">
+					                <div class="inner">
+					                    <h3><?=$o->jenis->nama?></h3>
+					                    <p>Waktu : <?=$o->waktu_pengerjaan?> Menit</p>
+					                </div>
+					                <div class="icon">
+					                    <i class="ion ion-bag"></i>
+					                </div>
+					                <?=Html::a($data['text'], $data['link'], $data['options']);?>
+					            </div>
+					        </div>
+					        <!-- ./col -->
+					        <?php endforeach?>
 	        <?php endif?>
     </div>
 
