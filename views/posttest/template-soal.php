40a82cad15adae2f56869f4199ea3081bd680210
Defri Indra Mahardika <defrindr@gmail.com>
2020-12-29 00:13:51 +0700

    menambahkan kembali timer yang terhapus

--- views/posttest/template-soal.php
+++ views/posttest/template-soal.php
@@ -3,6 +3,37 @@
 $this->title = (strpos($model->nama, "latihan") ? $model->nama : "Pelatihan {$model->nama}");
 
 $this->registerJs('
+var countDownDate = new Date("'. $pelatihan_soal_peserta->waktu_selesai. '").getTime();
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  let template = "";
+  if(days !== 0) template += days + "d ";
+  if(hours !== 0) template += hours + "h ";
+  if(minutes !== 0) template += minutes + "m ";
+  if(seconds !== 0) template += seconds + "s ";
+  document.getElementById("countdown").innerHTML = template;
+
+  if (distance < 0) {
+    clearInterval(x);
+    alert("Anda Kehabisan Waktu");
+    location.reload();
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);');
+
+$this->registerJs('
 let unique_id = "'. $model->unique_id. '";
 
 $(document).ready(function(){
